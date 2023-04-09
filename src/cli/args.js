a97ceb7d2ac0421805a365ab2f0d43fc87a2b9a5
andrejvanov42 <andrejvanov42@gmail.com>
2023-04-09 19:37:42 +0300

    fix: change format of output

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,6 +3,9 @@ const parseArgs = () => {
   const formattedArrayOfArgVal = [];
 
   while (arrayOfArgVal.length !== 0) {
+    arrayOfArgVal.map((val, index, arr) => {
+      if (val.startsWith("--")) arr[index] = val.slice(2);
+    });
     formattedArrayOfArgVal.push(arrayOfArgVal.splice(0, 2).join(" is "));
   }
   console.log(formattedArrayOfArgVal.join(", "));
