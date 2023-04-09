eaa184ae97209a4b513cd2d6d6cd1c997107266f
andrejvanov42 <andrejvanov42@gmail.com>
2023-04-09 19:38:30 +0300

    feat: implemet transform script

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const readble = process.stdin;
+  const writable = process.stdout;
+  const reverseText = new Transform({
+    transform(chunk, enc, callback) {
+      const chunkStringified = chunk.toString().trim();
+      const reversedText = chunkStringified.split("").reverse().join("");
+      callback(null, reversedText + "\n");
+    },
+  });
+  await pipeline(readble, reverseText, writable);
 };
 
 await transform();
